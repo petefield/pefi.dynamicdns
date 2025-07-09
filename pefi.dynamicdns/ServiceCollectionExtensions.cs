e4058becc095537154e7bfc9320764d451c16801
Pete Field <pete.field@gmail.com>
2025-07-09 05:07:18 +0100

    add configu

---
+++ pefi.dynamicdns/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using pefi.Rabbit;
+
+namespace pefi
+{
+    public static class ServiceCollectionExtensions
+    {
+
+
+        public class MessagingConfig()
+        {
+            public string Address { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+
+        }
+        public static IServiceCollection AddPeFiMessaging(this IServiceCollection services, Action<MessagingConfig> configureOptions)
+        {
+            services.Configure(configureOptions);
+
+            return services.AddSingleton<IMessageBroker>(sp => {
+
+                var options = sp.GetRequiredService<IOptions<MessagingConfig>>().Value;
+
+                var log = sp.GetRequiredService<ILogger<Program>>();
+
+                log.LogInformation($"Messaging : {options.Username} {options.Password}, {options.Address}");
+
+                return new MessageBroker(options.Address, options.Username, options.Password);
+
+            } );
+
+        }
+        public static IServiceCollection AddPeFiMessaging(this IServiceCollection services, string address, string username, string password)
+            => services.AddSingleton<IMessageBroker>(sp => new MessageBroker(address, username, password));
+
+
+    }
+
+}
